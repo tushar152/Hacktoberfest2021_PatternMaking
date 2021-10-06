4fde515225ff92172185b13f5f9420d052c80331
Souvik Mishra <58814221+souvikmishra@users.noreply.github.com>
2021-10-06 14:15:02 +0530

    Delete half_number_tree.py

--- Python/half_number_tree.py
+++
@@ -1,6 +0,0 @@
-# Python Numeric Pattern
-for num in range(10):
-for i in range(num):
-print (num, end=" ") #printing the number
-# We will use new line in order to display the pattern correctly
-print("\n")
