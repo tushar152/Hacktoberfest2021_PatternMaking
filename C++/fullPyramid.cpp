a3c78fe3c6197181581733d201f0f06742f17559
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2021-10-06 23:19:44 +0530

    Update fullPyramid.cpp

--- C++/fullPyramid.cpp
+++ C++/fullPyramid.cpp
@@ -1,30 +1,37 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-//this function will take input as size and prints the full pyramid
-//of size/length n as output
-void fullPyramid(int size) {
-	for(int row=1;row<=size;row++) {
-		for(int space=1;space<=size-row;space++) {
-			cout<<' '<<' ';
-		}
-		int num=row;
-		for(int inc=1;inc<=row;inc++) {
-			cout<<num<<' ';
-			num++;
-		}
-		num--;
-		for(int dec=1;dec<=row-1;dec++) {
-			num--;
-			cout<<num<<' ';
-		}
-		cout<<endl;
-	}
+void PyramidPattern1(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int k = n - i; k > 0; k--) {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+void PyramidPattern2(int n) {
+  for (int i = n; i >= 1; i--) {
+    for (int k = n - i; k > 0; k--) {
+      cout << " ";
+    }
+    for (int j = i; j > 0; j--) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
 }
 
 int main() {
-	int n;
-	cin>>n;
-	//calling the fullPyramid function 
-	fullPyramid(n);
+  int num;
+  cout << "Enter number of levels of the pattern :" << endl;
+  cin >> num;
+  PyramidPattern1(num);
+  cout << endl;
+  PyramidPattern2(num);
+
+  return 0;
 }
