0b3528ba81753103cb1638b322f64b5af94c5ff3
Parth Kholkute <pkholkute9@gmail.com>
2021-10-06 15:44:24 +0530

    Added Full Pyramid in C

--- Python/Half_Pyramid.py
+++ Python/Half_Pyramid.py
@@ -1,4 +1,5 @@
-n = int(input("Enter height of triangle: "))
-
-for i in range(n):
-    print('*'*(i+1))
+n=int(input())
+for i in range(n+1):
+    for _ in range(i):
+        print("*",end="")
+    print("")
