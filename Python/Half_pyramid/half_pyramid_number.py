6c9177ceb3c797d26c0d1b9623da2d1669a8272e
nitin <77408803+capt-doki@users.noreply.github.com>
2021-10-07 21:17:38 +0530

    half_pyramid_number_python
    #7

---
+++ Python/Half_pyramid/half_pyramid_number.py
@@ -0,0 +1,27 @@
+
+def numpat(n):
+     
+    
+    num = 1
+ 
+    
+    for i in range(0, n):
+     
+        
+        num = 1
+     
+       
+        for j in range(0, i+1):
+         
+                
+            print(num, end=" ")
+         
+          
+            num = num + 1
+     
+       
+        print("\r")
+ 
+
+n = int(input())
+numpat(n)
