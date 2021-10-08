82c70d52c471b4cc9f5ad41d16c1eb32c5e9617d
Tushar Garg <61476389+tushar152@users.noreply.github.com>
2021-10-08 16:08:39 +0530

    Create Special Pattern.java

---
+++ Java/Special Pattern.java
@@ -0,0 +1,19 @@
+public class SpecialPattern{
+    public static void main(String[] args){  
+        int rows=8;   
+        for (int i = 1; i <= rows; i++){  
+            for (int j = 1; j <= i; j++){  
+                System.out.print(j+" ");   
+            }   
+            System.out.println();   
+        }   
+        for (int i = rows-1; i >= 1; i--)  
+        {  
+            for (int j = 1; j <= i; j++) 
+            {  
+                System.out.print(j+" ");  
+            }  
+            System.out.println();  
+        }  
+    }  
+}  
